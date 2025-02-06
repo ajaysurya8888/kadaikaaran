60f08cecc7998459d692e5918c4c05909b248ada
Ajaysurya Mayureswaran <ajaysurya.m98@gmail.com>
2025-02-06 17:00:23 +0530

    add springboot initializr

---
+++ src/main/java/com/kadaikaaran/kadaikaaran/KadaikaaranApplication.java
@@ -0,0 +1,13 @@
+package com.kadaikaaran.kadaikaaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaikaaranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaikaaranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadaikaaran/kadaikaaran/KadaikaaranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadaikaaran.kadaikaaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaikaaranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
